12d7e1b5dfa1d5476ef11d771826450e968dabd2
japhheth <japhheth@gmail.com>
2019-08-11 21:35:15 +0100

    Initial commit

---
+++ style/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    document.getElementById("toggler").onclick = function() {
+      $('#nav-links').slideToggle('fas');
+    }
+
+    document.getElementById("btn1").onclick = function(){
+    document.getElementById("replace1").innerHTML = "Welcome to Inc, we deliver the best !"
+}
+
+});
+
+
+// window.onload = function(){
+        
+
+// }
+
